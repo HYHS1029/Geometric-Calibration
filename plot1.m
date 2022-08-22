01d3e97acc1b8f0ba55f557eb157ff355be9edd1
HYH1029 <j2286539@gmail.com>
2022-08-22 14:16:01 +0800

    plot

---
+++ plot1.m
@@ -0,0 +1,10 @@
+clc
+clear all
+displa=linspace(0,5,1000);
+Aref=1;
+Asam=1;
+I=(1/2)*(Aref^2)+(1/2)*(Asam^2)+(Aref*Asam)*cos((2*pi)/0.5*2*displa);
+line(displa,I)
+% title('Intensity versus mirror displacement');
+% xlabel('displacement(£gm)');
+% ylabel('Intensity');
